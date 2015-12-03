472e4d08ddceb38f1294930d03968a8fd61b2a91
Leo Arias <yo@elopio.net>
2015-12-03 01:01:31 -0600

    Set the HOME directory. May not work, but gets further.

--- build/ubuntu/cloud9.sh
+++ build/ubuntu/cloud9.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec node $SNAPP_APP_PATH/lib/node_modules/c9/server.js -w $SNAPP_APP_DATA_PATH
+HOME=$SNAPP_APP_DATA_PATH exec node $SNAPP_APP_PATH/lib/node_modules/c9/server.js -w $SNAPP_APP_DATA_PATH
